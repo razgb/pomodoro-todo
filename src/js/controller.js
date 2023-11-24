ee7f56877c271a30ce15c6825f9a526ed4b129dc
razgb <hamaraz2003@gmail.com>
2023-11-24 08:58:43 +0300

    Development v1.0.8: Menu slide completed

---
+++ src/js/controller.js
@@ -0,0 +1,8 @@
+import view from "./views/view.js";
+// FIX THIS WHEN YOU WAKE UP PLEASE.
+
+const init = function () {
+  view.addHandlerMenu();
+};
+
+init();
--- src/js/views/view.js
+++ src/js/views/view.js
@@ -1,9 +1,30 @@
 class view {
   _parentContainer = "";
+  _menuState = false;
 
   render() {}
 
   _clear() {
     this._parentContainer.innerHTML = "";
   }
+
+  addHandlerMenu() {
+    const menuButton = document.querySelector(".menu__hamburger");
+    menuButton.addEventListener("click", function () {
+      const menuTab = document.querySelector(".menu");
+      const app = document.querySelector(".app");
+
+      if (!this._menuState) {
+        menuTab.style.transform = "translateX(0)";
+        app.style.marginLeft = "20%";
+        this._menuState = true;
+      } else {
+        menuTab.style.transform = "translateX(-110%)";
+        app.style.marginLeft = "0";
+        this._menuState = false;
+      }
+    });
+  }
 }
+
+export default new view();
