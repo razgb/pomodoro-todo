24f63105f8383fc9bdea01117dd13b4b1f915419
Raz Neaiz <raz@Razs-MacBook-Pro.local>
2023-12-12 15:30:48 +0300

    Development v1.4.2 timer speed fix

--- src/js/timerWorker.js
+++ src/js/timerWorker.js
@@ -1,36 +1,36 @@
-onmessage = function(e) {
-    let seconds = e.data.seconds; 
-    let timerState = e.data.timerState; 
-    let currentMode = e.data.currentMode;    
+onmessage = function (e) {
+  let seconds = e.data.seconds;
+  let timerState = e.data.timerState;
+  let currentMode = e.data.currentMode;
 
-    let currentLoop = 0; 
+  let currentLoop = 0;
 
-    let timer = setInterval(() => {
-        if (!timerState) {
-            clearInterval(timer); 
-            close(); 
-            console.log('Timer has been closed due to false timer state');
-            return; 
-        }
+  let timer = setInterval(() => {
+    if (!timerState) {
+      clearInterval(timer);
+      close();
+      console.log("Timer has been closed due to false timer state");
+      return;
+    }
 
-        if (seconds === 0) return; 
+    if (seconds === 0) return;
 
-        --seconds; 
-        if (seconds === 0 && currentLoop === 0) {
-            if (currentMode === 'POMODORO') {
-                currentMode = 'SHORT BREAK';
-            } else if (currentMode === 'SHORT BREAK') {
-                currentMode = 'POMODORO'; 
-            }
-            //  else if (currentMode === 'LONG BREAK') {
-            // }
-            currentLoop++;  
-        } 
+    --seconds;
+    if (seconds === 0 && currentLoop === 0) {
+      if (currentMode === "POMODORO") {
+        currentMode = "SHORT BREAK";
+      } else if (currentMode === "SHORT BREAK") {
+        currentMode = "POMODORO";
+      }
+      //  else if (currentMode === 'LONG BREAK') {
+      // }
+      currentLoop++;
+    }
 
-        postMessage({
-            seconds: seconds,
-            timerState: timerState,
-            currentMode: currentMode,
-        }); 
-    }, 100);
-}
+    postMessage({
+      seconds: seconds,
+      timerState: timerState,
+      currentMode: currentMode,
+    });
+  }, 1000);
+};
